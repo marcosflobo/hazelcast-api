cd3ae1a033a0f22253cc328fd06c5523e2ae21a6
Marcos F. Lobo <hello@marcosflobo.com>
2020-11-01 16:26:43 +0100

    Add HAZELCAST_CLIENT_CLUSTERNAME parameter

--- src/main/java/com/marcosflobo/hazelcast/api/hazelcast/HazelcastFactory.java
+++ src/main/java/com/marcosflobo/hazelcast/api/hazelcast/HazelcastFactory.java
@@ -1,8 +1,5 @@
 package com.marcosflobo.hazelcast.api.hazelcast;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.config.ClientNetworkConfig;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
@@ -10,12 +7,12 @@ import com.hazelcast.core.HazelcastInstance;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Value;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Factory
 public class HazelcastFactory {
+
+  @Value("${hazelcast.client.clustername}")
   private String hazelcastClusterName;
 
   @Value("${hazelcast.network.addresses}")
@@ -25,15 +22,14 @@ public class HazelcastFactory {
   private String hazelcastClientName;
 
   @Bean
-  public HazelcastInstance getHazelcastClient () {
+  public HazelcastInstance getHazelcastClient() {
     NetworkConfig networkConfig = new NetworkConfig();
     networkConfig.setPublicAddress(hazecastNetworkAddresses.get(0));
     Config config = new Config();
     config.setInstanceName(hazelcastClientName);
-    config.setClusterName("dev");
+    config.setClusterName(hazelcastClusterName);
     config.setNetworkConfig(networkConfig);
 
     return Hazelcast.newHazelcastInstance(config);
-
   }
 }
