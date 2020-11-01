6546036f6a350066b888d4f987af29341bf4a1c3
Marcos F. Lobo <hello@marcosflobo.com>
2020-11-01 17:19:52 +0100

    Improve code for MapService and usage of Hazelcast client

--- src/main/java/com/marcosflobo/hazelcast/api/service/MapServiceImpl.java
+++ src/main/java/com/marcosflobo/hazelcast/api/service/MapServiceImpl.java
@@ -1,6 +1,7 @@
 package com.marcosflobo.hazelcast.api.service;
 
 
+import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import com.marcosflobo.hazelcast.api.domain.MapPutRequest;
 import com.marcosflobo.hazelcast.api.hazelcast.HazelcastFactory;
@@ -10,15 +11,17 @@ import javax.inject.Singleton;
 @Singleton
 public class MapServiceImpl implements MapService {
 
+  private HazelcastInstance hazelcastInstance;
   private IMap<String, String> hazelcastMap;
 
   public MapServiceImpl(HazelcastFactory hazelcastFactory) {
-    hazelcastMap = hazelcastFactory.getHazelcastClient().getMap("mymap");
+    hazelcastInstance = hazelcastFactory.getHazelcastClient();
   }
 
   @Override
   public HttpResponse<String> write(MapPutRequest mapPutRequest) {
 
+    hazelcastMap = hazelcastInstance.getMap("mymap");
     hazelcastMap.putAsync(mapPutRequest.getKey(), mapPutRequest.getValue());
 
     return HttpResponse.ok();
