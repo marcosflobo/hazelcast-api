7afeba75b9c3773554383757eaf53367410b861e
Marcos F. Lobo <hello@marcosflobo.com>
2020-11-02 19:50:06 +0100

    Solve syntax for Groovy validator

--- src/test/groovy/com/marcosflobo/hazelcast/api/hazelcast/HazelcastFactorySpec.groovy
+++ src/test/groovy/com/marcosflobo/hazelcast/api/hazelcast/HazelcastFactorySpec.groovy
@@ -1,8 +1,6 @@
 package com.marcosflobo.hazelcast.api.hazelcast
 
 import com.hazelcast.client.config.ClientConfig
-import com.hazelcast.client.config.ClientNetworkConfig
-import com.hazelcast.core.HazelcastInstance
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -16,17 +14,17 @@ class HazelcastFactorySpec extends Specification {
     HazelcastFactory hazelcastFactory = new HazelcastFactory()
 
     @Unroll
-    def "The Hazelcast configuration is properly created"(){
+    def "the hazelcast configuration is properly created"(){
         when: "The Hazelcast client config is created"
         ClientConfig clientConfig = hazelcastFactory.getClientConfig()
 
         then: "It should have the Instance name which is set on the application-test.yml"
-        clientConfig.getInstanceName() == "hazelcastApi-test"
+        clientConfig.instanceName == "hazelcastApi-test"
 
         and: "It should have the Network Addresses which is set on the application-test.yml"
-        clientConfig.getNetworkConfig().getAddresses().get(0) == "localhost-test:5701"
+        clientConfig.networkConfig.getAddresses().get(0) == "localhost-test:5701"
 
         and: "It should have the Cluster Name which is set on the application.yml"
-        clientConfig.getClusterName() == "dev"
+        clientConfig.clusterName == "dev"
     }
 }
